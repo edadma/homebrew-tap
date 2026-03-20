9674f0511ab38be5cffec56e96d1c95e67a7af5a
Edward A Maxedon <edadma@gmail.com>
2026-03-20 08:09:36 -0400

    Update Roamer to 0.0.10

--- Casks/roamer.rb
+++ Casks/roamer.rb
@@ -1,6 +1,6 @@
 cask "roamer" do
-  version "0.0.9"
-  sha256 "cc9a3c15d4969fdfec5cc931b347017002c69165ca52f05d09f1016c119a3e4d"
+  version "0.0.10"
+  sha256 "e6d5970b29409ab6447eca39d25696d15cd7655e86c7e48dca0a0a59174064cc"
 
   url "https://github.com/edadma/roamer/releases/download/v#{version}/Roamer-#{version}-arm64.dmg"
   name "Roamer"
